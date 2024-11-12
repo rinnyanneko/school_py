f6d20d8819581979ba2b54ec2432631f9d4b3f8d
凛喵 <rinnyanneko@outlook.com>
2024-11-12 09:50:41 +0800

    2024-11-12

--- pygame/main.py
+++ pygame/main.py
@@ -1,16 +1,19 @@
 import pygame
+import os
 
 WIDTH = 1280
 HEIGHT = 720
 pygame.init()
 pygame.display.set_caption("Hello World!")
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
+background = pygame.image.load(os.path.join("assets", "bliss.jpg"))
+background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 run:bool = True
 
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    screen.fill((255, 255, 0))
+    screen.fill(114514)
+    screen.blit(background, (0, 0))
     pygame.display.update()
